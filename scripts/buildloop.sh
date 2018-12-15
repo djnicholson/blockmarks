5f3a9b30e5e3494d22c838368dc160ec6b6382d9
David Nicholson <me@djnicholson.com>
2018-12-15 19:26:50 +0000

    more efficient build loop

--- scripts/buildloop.sh
+++ scripts/buildloop.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+ROOT=$DIR/..
+APPROOT=$ROOT/app
+BUILDROOT=$ROOT/build
+DEPLOYROOT=$ROOT/deploy
 
+chsum1=""
 while [[ true ]]
 do
-    $DIR/build/build.sh
+    chsum2=`find $APPROOT -type f -exec md5 {} \;`
+    if [[ $chsum1 != $chsum2 ]] ; then      
+        $DIR/build/build.sh
+        chsum1=$chsum2
+    fi
     sleep 3
 done
